7b77b50d3b6c110b07f49c39ca408e979fea4e51
Ivo Meixner <natiwastaken@gmail.com>
2020-01-03 22:30:03 +0100

    Update generated main.c/h code

--- tmp/main.h
+++ tmp/main.h
@@ -10,15 +10,23 @@
 #line 2 "src/main.func"
 #include <stdbool.h>
 #line 3 "src/main.func"
+#include <stdarg.h>
+#line 4 "src/main.func"
+#include "func.tab.h"
+#line 5 "src/main.func"
 #include "strlist.h"
 
-#line 21 "src/main.func"
+#line 23 "src/main.func"
 int main(int argc, char** argv);
-#line 123 "src/main.func"
+#line 125 "src/main.func"
 bool strsuffix(char* str, char* suffix);
-#line 130 "src/main.func"
+#line 132 "src/main.func"
 char* getFileName(char* path);
-#line 142 "src/main.func"
+#line 144 "src/main.func"
 char* sanitizePath(char* path);
+#line 157 "src/main.func"
+char* strformat(char* format, ...);
+#line 168 "src/main.func"
+void yyerror(const char* err);
 
 #endif
